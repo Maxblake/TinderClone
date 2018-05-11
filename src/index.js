98901a195df6d9bf3dc435ab2da47861d11a3e0b
Holger_Tidemand <tidemand.holger@gmail.com>
2018-05-11 11:06:53 +0200

    Restruct project for better readyability;

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Home from './screens/home'
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWq2pA1ZLdmjRi0MFpSRccKf_V98HW1BQ',
+  databaseURL: 'https://tinderclone-5c307.firebaseio.com',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default class App extends Component {
+  render() {
+    return <Home />
+  }
+}
