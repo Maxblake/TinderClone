218220e31bf5ede0da9c5852d5425907af994bdb
Holger_Tidemand <tidemand.holger@gmail.com>
2018-05-11 10:38:34 +0200

    Make use of data from database to display our card stack;

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ firebase.initializeApp(firebaseConfig)
 export default class App extends Component {
   state = {
     profileIndex: 0,
+    profiles: [],
   }
 
   componentWillMount() {
@@ -21,7 +22,13 @@ export default class App extends Component {
       .ref()
       .child('users')
       .once('value', snap => {
-        console.log('data', snap)
+        let profiles = []
+        snap.forEach(profile => {
+          const { name, bio, birthday, id } = profile.val()
+          console.log(bio)
+          profiles.push({ name, bio, birthday, id })
+        })
+        this.setState({ profiles })
       })
   }
 
@@ -31,7 +38,7 @@ export default class App extends Component {
 
   _renderCards() {
     const { profileIndex } = this.state
-    return profiles
+    return this.state.profiles
       .slice(profileIndex, profileIndex + 3)
       .reverse()
       .map((profile, i) => {
@@ -44,48 +51,3 @@ export default class App extends Component {
     return <View style={{ flex: 1 }}>{this._renderCards()}</View>
   }
 }
-
-const profiles = [
-  {
-    id: '259389830744794',
-    name: 'Candice',
-    birthday: '10/18/1986',
-    bio: 'Supermodel',
-  },
-  {
-    id: '720115413',
-    name: 'Alessandra',
-    birthday: '1/10/1989',
-    bio: 'Dancer',
-  },
-  {
-    id: '169571172540',
-    name: 'Miranda',
-    birthday: '12/12/1983',
-    bio: 'Doctor',
-  },
-  {
-    id: '1476279359358140',
-    name: 'Alissa',
-    birthday: '2/11/1990',
-    bio: 'Comedian',
-  },
-  {
-    id: '1140849052644433',
-    name: 'Behati',
-    birthday: '3/23/1991',
-    bio: 'Developer',
-  },
-  {
-    id: '912478262117011',
-    name: 'Rosie',
-    birthday: '9/4/1989',
-    bio: 'Artist',
-  },
-  {
-    id: '173567062703796',
-    name: 'Kendall',
-    birthday: '8/17/1992',
-    bio: 'Truck Driver',
-  },
-]
