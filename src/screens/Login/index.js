8f40276246eb7131c4c739a84f92150ce60183c9
Holger_Tidemand <tidemand.holger@gmail.com>
2018-05-14 08:27:14 +0200

    add facebook login functionallity

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -5,12 +5,27 @@ import FacebookButton from '../../components/FacebookButton'
 import styles from './styles'
 
 export default class Login extends Component {
+  _login = async () => {
+    const APP_ID = '599138407119061'
+    const options = {
+      permissions: ['public_profile'],
+    }
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      APP_ID,
+      options
+    )
+
+    if (type === 'success') {
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      )
+      console.log(await response.json())
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <FacebookButton
-          onPress={() => console.log('Heaven is a place on earth')}
-        />
+        <FacebookButton onPress={this._login} />
       </View>
     )
   }
